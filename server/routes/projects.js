06d7c58ee238bbb177e9951c54687f3e2ffdbe56
Raphael Cordero <68399853+rafogi@users.noreply.github.com>
2020-11-20 12:13:39 -0800

    fix bugs on date parse and picture

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -12,7 +12,7 @@ router.route('/add').post((req, res) => {
   const description = req.body.description;
   const picture = req.body.picture;
   const url = req.body.url;
-  const monthYear = Date.parse(req.body.monthYear);
+  const monthYear = req.body.monthYear;
 
   const newProject = new Project({
     projectName,
@@ -45,7 +45,7 @@ router.route('/update/:id').post((req, res) => {
       project.projectName = req.body.projectName;
       project.description = req.body.description;
       project.picture = req.body.picture;
-      project.monthYear = Date.parse(req.body.monthYear);
+      project.monthYear = req.body.monthYear;
 
       exercise.save()
         .then(() => res.json('project updated!'))
