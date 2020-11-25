568f4712d324e504bf50a25c8cb2c436c56e8a81
Raphael Cordero <68399853+rafogi@users.noreply.github.com>
2020-11-25 12:15:26 -0800

    add animation to home page

--- front-end/src/components/ProjectList.js
+++ front-end/src/components/ProjectList.js
@@ -9,7 +9,10 @@ function ProjectList(props) {
       
     <div className="project-article">
         <a className="proj-link" href={props.url}>
-        <h3 className="projHead">{props.name}</h3>
+        <div className="bar">
+            <h3 className="projHead">{props.name}</h3>
+            <i></i>
+		</div>
         <img className="project-image" src={props.picture} alt="projpic"/>
         <div className="notes">
             <p className="proj-desc">{props.description}</p>
