20d5577f33bc000bc35b94f53b75be08422c948d
Raphael Cordero <68399853+rafogi@users.noreply.github.com>
2020-11-26 19:11:07 -0800

    add skills to page

--- front-end/src/components/Skills.js
+++ front-end/src/components/Skills.js
@@ -1,11 +1,45 @@
 import React from 'react';
-
+import { Spring } from 'react-spring/renderprops';
+import "./Skills.css";
 
 function Skills() {
   return (
-    <div className="Skills">
-        <h1>Skills</h1>
-    </div>
+    <Spring 
+    from={{opacity:0, marginLeft:-500}}
+    to={{opacity:1,marginLeft:-0}}
+    > 
+      {props => (
+        <div style={props} className="skills">
+          <h1>Skills</h1>
+          <h2>Web Development</h2>
+          <h3>Languages</h3>
+          <ul>
+            <li>JavaScript</li>
+            <li>Ruby</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>C/C++</li>
+          </ul>
+          <h4>Frameworks</h4>
+          <ul>
+            <li>React</li>
+            <li>JQuery</li>
+            <li>NodeJs</li>
+            <li>RubyOnRails</li>
+            <li>ExpressJS</li>
+            <li>PHP</li>
+            <li>XAMPP</li>
+            <li>Bootstrap</li>
+          </ul>
+          <h4>Databases</h4>
+          <ul>
+            <li>PostgresSql</li>
+            <li>MySql</li>
+            <li>MongoDB</li>
+          </ul>
+        </div>
+      )}
+     </Spring> 
   );
 }
 
