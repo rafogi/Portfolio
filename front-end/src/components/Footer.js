df70c586827a1697f0f3bec7acd64606105da449
Raphael Cordero <68399853+rafogi@users.noreply.github.com>
2020-11-27 09:43:26 -0800

    fix body margins

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -5,10 +5,10 @@ import "./Footer.css"
 export default function Footer() {
     return (
         <section className="Footer">
-            <section>
+            <section className="footer-icons">
             <SocialIcon url="https://www.linkedin.com/in/raffy-cordero-1857a0131" />
             <SocialIcon url="https://github.com/rafogi" />
-
+            <SocialIcon url="https://www.instagram.com/raficturetime/" />
             </section>
         </section>
     )
