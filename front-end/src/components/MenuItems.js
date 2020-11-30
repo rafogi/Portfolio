64b556c190328d933f55755cde82242187bc54c6
Raphael Cordero <68399853+rafogi@users.noreply.github.com>
2020-11-30 11:38:01 -0800

    add hamburger menu

---
+++ front-end/src/components/MenuItems.js
@@ -0,0 +1,23 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '/',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Skills',
+        url: '/Skills',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Projects',
+        url: 'Projects',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact',
+        url: 'Contact',
+        cName: 'nav-links'
+    }
+
+]
--- front-end/src/components/Nav.js
+++ front-end/src/components/Nav.js
@@ -1,26 +1,59 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {NavLink}  from 'react-router-dom';
+import { MenuItems } from "./MenuItems"
 import "./nav.css";
 
-function Nav() {
-  return (
-    <nav className="nav-bar">
-    <ul className="nav-info">
-      <NavLink exact={true} to="/" activeClassName="nav-info-clicked">
-        <li>Home</li>
-      </NavLink>
-      <NavLink exact={true} to="/skills" activeClassName="nav-info-clicked">
-        <li>Skills</li>
-      </NavLink>
-      <NavLink exact={true} to="/projects" activeClassName="nav-info-clicked">
-        <li>Projects</li>
-      </NavLink>
-      <NavLink exact={true} to="/contact" activeClassName="nav-info-clicked">
-        <li>Contact Me</li>
-      </NavLink>
-    </ul>
-  </nav>
-  );
+// function Nav() {
+//   return (
+//     <nav className="nav-bar">
+//     <ul className="nav-info">
+//       <NavLink exact={true} to="/" activeClassName="nav-info-clicked">
+//         <li>Home</li>
+//       </NavLink>
+//       <NavLink exact={true} to="/skills" activeClassName="nav-info-clicked">
+//         <li>Skills</li>
+//       </NavLink>
+//       <NavLink exact={true} to="/projects" activeClassName="nav-info-clicked">
+//         <li>Projects</li>
+//       </NavLink>
+//       <NavLink exact={true} to="/contact" activeClassName="nav-info-clicked">
+//         <li>Contact Me</li>
+//       </NavLink>
+//     </ul>
+//   </nav>
+//   );
+// }
+
+// export default Nav;
+
+class Navbar extends Component {
+    state = { clicked: false }
+
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
+
+    render() {
+        return(
+            <nav className="NavbarItems">
+                <h1 className="navbar-logo">Raffy</h1>
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a className={item.cName} href={item.url}>
+                                {item.title}
+                                </a>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </nav>
+        )
+    }
 }
 
-export default Nav;
+export default Navbar
