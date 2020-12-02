709921e7a054d549f45ac0d222137ac3ddd776f7
rafogi <raffycordero2001@yahoo.ca>
2020-12-01 21:10:37 -0800

    change env var

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = ATLAS_URI
+const uri = process.env.ATLAS_URI
 mongoose.connect(uri, {useNewUrlParser:true, useCreateIndex:true }
 );
 
