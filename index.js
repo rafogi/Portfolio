b92ed2dfac2351c7f649386f5994fd8d9ec2405f
rafogi <raffycordero2001@yahoo.ca>
2020-12-01 19:30:19 -0800

    reorganize files

--- index.js
+++ index.js
@@ -26,7 +26,14 @@ const projects = require('./routes/projects');
 
 app.use('/api/projects', projects);
 
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    });
+  }
 
+  
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
 })
