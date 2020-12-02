61870bf91fe1b3d646c247dea743fd544d0126d8
rafogi <raffycordero2001@yahoo.ca>
2020-12-01 20:32:55 -0800

    change path directory

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ const projects = require('./routes/projects');
 app.use('/api/projects', projects);
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-    app.use(express.static('client/build'));
+    app.use(express.static('front-end/build'));
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname + '/client/build/index.html'));
+      res.sendFile(path.join(__dirname + '/front-end/build/index.html'));
     });
   }
 
